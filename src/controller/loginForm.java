bf204c82384726fe62dfd376771c73251c2deaef
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-08-14 14:05:13 +0530

    chat app ui completed

--- src/controller/loginForm.java
+++ src/controller/loginForm.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class loginForm {
+    public AnchorPane loginPage;
+
+    public void logInOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/chatPage.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) loginPage.getScene().getWindow();
+        window.setTitle("Live Chat");
+        window.setMaximized(true);
+        window.setScene(new Scene(load));
+    }
 }
